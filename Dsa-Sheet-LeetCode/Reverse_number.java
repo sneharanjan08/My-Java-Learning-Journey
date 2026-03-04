6806fb716f901439c6cae5427f592156a5456daf
Sneha Ranjan <sneha2006ranjan@gmail.com>
2026-03-04 14:58:08 +0530

    Dsa-Sheet-LeetCode

---
+++ Dsa-Sheet-LeetCode/Reverse_number.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Reverse_number {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        while (n!=0) {
+            int rem=n%10;
+            System.out.println(rem);
+            n=n/10;
+        }
+    }
+}
